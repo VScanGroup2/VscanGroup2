1555b4b0114ab4a49353a9cc6b6ff3a8647b66f0
VScanGroup2 <vscangroup@gmail.com>
2025-10-12 20:32:20 +0800

    Create Calendar.js

---
+++ src/Pages/Calendar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Calendar = () => {
+  return (
+    <div className="calendar-container">
+      <h2>Calendar</h2>
+      <p>Calendar component</p>
+    </div>
+  );
+};
+
+export default Calendar;
